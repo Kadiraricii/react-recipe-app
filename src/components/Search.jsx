aeeac57fa5021b0bea1babd635b6db9cba465dc9
Kadir Arıcı <mehmetkadira0@gmail.com>
2025-08-05 00:30:18 +0300

    Clean up formatting in Search.js

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,31 +1,31 @@
 import styled from "styled-components";
-import {useState } from "react";
-import {FaSearch} from "react-icons/fa";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 function Search() {
-    const [input, setInput] = useState("")  
-     const navigate = useNavigate();
+    const [input, setInput] = useState("")
+    const navigate = useNavigate();
 
-    const submitHandler = (e) =>{
+    const submitHandler = (e) => {
         e.preventDefault();
         navigate("/searched/" + input)
 
     };
-  return (
-    <FormStyle onSubmit={submitHandler}>
-    <div>
-    <FaSearch></FaSearch>
-        <input 
-        onChange={(e) => setInput(e.target.value)} 
-        type= "text"
-         value={input}
-            
-         />
-    
-    </div>
-    </FormStyle>
-  )
+    return (
+        <FormStyle onSubmit={submitHandler}>
+            <div>
+                <FaSearch></FaSearch>
+                <input
+                    onChange={(e) => setInput(e.target.value)}
+                    type="text"
+                    value={input}
+
+                />
+
+            </div>
+        </FormStyle>
+    )
 }
 
 
